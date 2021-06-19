648c823395c37e0c65efaea8e3557054487892ba
techpro-education <riyaz.techproed@gmail.com>
2021-06-19 15:56:09 +0400

    About us

--- src/home/Home.js
+++ src/home/Home.js
@@ -3,14 +3,37 @@ import "./Home.css";
 import Product from "../product/Product.js";
 import Price from "../price/Price.js";
 import About from "../about/About.js";
+import silver from "../images/card/silver.jpg";
+import gold from "../images/card/gold.jpg";
+import platinum from "../images/card/platinum.jpg";
 import Banner from "../banner/Banner.js";
 function Home() {
   return (
     <div className="home">
       <Banner />
       <About />
-      <div id="product">
-        <Product />
+      <div id="product" className="home__row">
+        <Product
+          id="1231245"
+          benefit="Basic package with interactive features. Enjoy up to 20% discount at select restaurants.You don’t have to pay any additional fee."
+          type="Silver Card"
+          price={300}
+          image={silver}
+        />
+        <Product
+          id="1231250"
+          benefit="Bank of States has partnered with leading travel providers to bring offers on airline tickets, hotel stays, car hire and more."
+          type="Gold Card"
+          price={500}
+          image={gold}
+        />
+        <Product
+          id="1231255"
+          benefit="Get up to 5 supplementary cards for your family members. You don’t have to pay any additional fee and can choose the spending limit on each card."
+          type="Platinum Card"
+          price={800}
+          image={platinum}
+        />
       </div>
       <Price />
     </div>
--- src/product/Product.js
+++ src/product/Product.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
-  return <div className="product">Product Comes Here</div>;
-}
+const Product = ({ id, benefit, type, price, image }) => {
+  return (
+    <div className="product">
+      <div className="div product__info">
+        <h1>{type}</h1>
+        <p>{benefit}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+      </div>
+      <img className="card_img" src={image} alt="" width="25%" />
+    </div>
+  );
+};
 
 export default Product;
