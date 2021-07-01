91798c9324852b78af0063ee7ddb3dffa47295d5
techpro-education <riyaz.techproed@gmail.com>
2021-07-01 14:15:16 +0400

    Price

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,11 +1,134 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.png";
 import "./Footer.css";
-
+import TwitterIcon from "@material-ui/icons/Twitter";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import PinterestIcon from "@material-ui/icons/Pinterest";
 function Footer() {
   return (
-    <div className="footer" id="contactus">
-      Footer will come here
+    <div>
+      <footer className="footer-area">
+        <div className="container">
+          <div className="footer-inner">
+            <div className="row">
+              <div className="col-lg-3 col-md-6">
+                <div className="widget">
+                  <Link className="widget-title" to={"/"}>
+                    <img className="footer__logo" src={logo} alt="logo" />
+                    MTD Bank
+                  </Link>
+                  <ul className="widget-list">
+                    <li>112, West road, F1 456 Tram Town.</li>
+                    <li>Office No 2312</li>
+                    <li>info@mtdbank.com</li>
+                    <li>234-3434567</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-xl-2 col-lg-3 col-md-6 offset-lg-1">
+                <div className="widget widget-links">
+                  <div className="widget-title">Links</div>
+                  <ul className="widget-list">
+                    <li>
+                      <Link to={"/services"}>FAQs</Link>
+                    </li>
+                    <li>
+                      <Link to={"/about"}>About Us</Link>
+                    </li>
+                    <li>
+                      <Link to={"/contact"}>Contact Us</Link>
+                    </li>
+                    <li>
+                      <Link to={"/"}>Home</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-xl-2 col-lg-3 col-md-6">
+                <div className="widget widget-about">
+                  <h4 className="widget-title">About Us.</h4>
+                  <ul className="widget-list">
+                    <li>
+                      <Link to={"/about"}>About Us</Link>
+                    </li>
+                    <li>
+                      <Link to={"/blog"}>Blog</Link>
+                    </li>
+                    <li>
+                      <Link to={"/contact"}>Contact Us</Link>
+                    </li>
+                    <li>
+                      <Link to={"/"}>Sign in</Link>
+                    </li>
+                    <li>
+                      <Link to={"/"}>My Account</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6">
+                <div className="widget widget-contact">
+                  <h4 className="widget-title">Contact Us.</h4>
+                  <p>Please connect with us through following channels</p>
+                  <ul className="social-area">
+                    <li>
+                      <Link to={"/"}>
+                        <TwitterIcon />
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to={"/"}>
+                        <FacebookIcon />
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to={"/"}>
+                        <LinkedInIcon />
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to={"/"}>
+                        <PinterestIcon />
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div className="footer-bottom">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 align-self-center">
+              <div className="text-lg-left text-center">
+                <ul>
+                  <li>
+                    <Link to={"/"}>Terms & Conditions</Link>
+                  </li>
+                  <li>
+                    <Link to={"/"}>Privacy Policy</Link>
+                  </li>
+                  <li>
+                    <Link to={"/"}>Sitemap</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-lg-6 align-self-center">
+              <div className="copy-right text-lg-right text-center">
+                @ 2020, MTD Bank all right reserved
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* footer area end */}
     </div>
   );
 }
+
 export default Footer;
--- src/price/Price.js
+++ src/price/Price.js
@@ -1,9 +1,111 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Price.css";
+
 function Price() {
   return (
-    <div className="price" id="package">
-      Packages Comes here
+    <div className="pricing-area">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-8 text-center">
+            <div className="section-title">
+              <h6 className="subtitle subtitle-thumb">Our Packages</h6>
+              <h2 className="title">Grab Our Package</h2>
+              <p>
+                The right banking package for you . Get the privilege of
+                choosing between our many Banking Packages, and enjoy services
+                that will reward you and fit your lifestyle.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-lg-4 col-md-6">
+            <div className="single-price text-center">
+              <div className="pricing-details">
+                <h2 className="pricing-cost">5.50%</h2>
+                <h6 className="pricing-subtitle">For a month</h6>
+                <h4 className="pricing-title">Basic</h4>
+              </div>
+              <ul className="pricing-list">
+                <li>
+                  <Link to={"/services"}>Miximum Deposit $10,00</Link>
+                </li>
+                <li>
+                  <Link to={"/services"}>Minimum Deposit $10</Link>
+                </li>
+                <li>
+                  <Link to={"/services"}>Up to 50 Users available</Link>
+                </li>
+                <li>
+                  <Link to={"/services"}>Free Banking</Link>
+                </li>
+                <li>
+                  <Link className="btn btn-blue" to={"/services"}>
+                    Buy Now
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6">
+            <div className="single-price text-center">
+              <div className="pricing-details">
+                <h2 className="pricing-cost">7.50%</h2>
+                <h6 className="pricing-subtitle">For a month</h6>
+                <h4 className="pricing-title">Premium</h4>
+              </div>
+              <ul className="pricing-list">
+                <li>
+                  <Link to={"/services"}>Miximum Deposit $10,00</Link>
+                </li>
+                <li>
+                  <Link to={"/services"}>Minimum Deposit $10</Link>
+                </li>
+                <li>
+                  <Link to={"/services"}>Up to 50 Users available</Link>
+                </li>
+                <li>
+                  <Link to={"/services"}>Free Banking</Link>
+                </li>
+                <li>
+                  <Link className="btn btn-blue " to={"/services"}>
+                    Buy Now
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6">
+            <div className="single-price text-center">
+              <div className="pricing-details">
+                <h2 className="pricing-cost">9.50%</h2>
+                <h6 className="pricing-subtitle">For a month</h6>
+                <h4 className="pricing-title">Advanced Plan</h4>
+              </div>
+              <ul className="pricing-list">
+                <li>
+                  <Link to={"/services"}>Miximum Deposit $10,00</Link>
+                </li>
+                <li>
+                  <Link to={"/services"}>Minimum Deposit $10</Link>
+                </li>
+                <li>
+                  <Link to={"/services"}>Up to 50 Users available</Link>
+                </li>
+                <li>
+                  <Link to={"/services"}>Free Banking</Link>
+                </li>
+                <li>
+                  <Link className="btn btn-blue " to={"/services"}>
+                    Buy Now
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
